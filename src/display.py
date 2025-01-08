JLZ22/Rugby-Infinite-Passing

commit c262939c750bb89d49a0c03fe0b0cc1a622832f0

allow user to pass in config files to specify custom line configurations and object colors

src/display.py
@@ -20,8 +20,7 @@ class Display():
         lines: list[list[Player]],
         speed: int, 
         win_size: tuple[int, int], 
-        player_tints: dict[int, RGB | RGBA],
-        ball_tint: RGB | RGBA,
+        obj_tints: dict[int, RGB | RGBA],
         bg_color: RGB | RGBA,
         frame_rate: int, 
         step: bool,
@@ -34,14 +33,14 @@ class Display():
             lines (list[list[Player]]): A list of lines with players.
             speed (int): The rate at which sprites move.
             win_size (tuple[int, int]): The size of the window.
-            player_tints (dict[int, RGB  |  RGBA]): A dictionary of player ids and their respective tints if any. Empty dict will result in using default png colors.
-            ball_tint (RGB | RGBA): The tint of the ball. None will result in using default png color.
+            obj_tints (dict[int, RGB  |  RGBA]): A dictionary of player ids and their respective tints if any. Empty dict will result in using default png colors.
             bg_color (RGB | RGBA): The background color of the window.
             frame_rate (int): The frame rate of the simulation.
-            step (bool): Whether to run the simulation in step mode. This will pause the simulation after each pass.
-            User must press space to continue.
+            step (bool): Whether to run the simulation in step mode. This will pause the simulation after each pass. User must press space to continue.
             debug (bool): Whether to display the id of players instead of the image.
         '''
+        self.verify_obj_tints(obj_tints)
+        
         pygame.init()
         pygame.event.set_allowed([QUIT, KEYDOWN, KEYUP])
         self.clock = pygame.time.Clock()
@@ -52,7 +51,7 @@ class Display():
         
         self.lines = lines
         self.start_line = start_line
-        self.players, self.player_dict, self.ball = self._init_objects(player_tints, ball_tint)
+        self.players, self.player_dict, self.ball = self._init_objects(obj_tints)
         self.ball_moving = False
         self.moving_players = set()
         self.bg = bg_color
@@ -157,54 +156,58 @@ class Display():
         
     def _init_objects(
         self, 
-        player_tints: dict[int, RGB | RGBA], 
-        ball_tint: RGB | RGBA | None
+        obj_tints: dict[int, RGB | RGBA]
     ) -> tuple[set[DisplayObject], dict[int, DisplayObject], DisplayObject]:
         '''Create all the game objects for the simulation. A variable 
         number of players and one ball.
 
         Args:
-            player_tints (dict[int, RGB  |  RGBA]): The tints of the players. This can be empty
+            obj_tints (dict[int, RGB  |  RGBA]): The tints of the players. This can be empty
             which will result in using the default png colors.
-            ball_tint (RGB | RGBA | None): The tint of the ball. None will result in using the default png color.
 
         Returns:
             tuple[list[DisplayObject], dict[int, DisplayObject], DisplayObject]: A list of players, a dict that keeps track of each player's pid, and the ball.
         '''
-        num_players = sum([len(line) for line in self.lines])
-        num_lines = len(self.lines)
         player_dict = {}
         players = pygame.sprite.Group()
-        
-        pid = 0
-        
-        for row in range(len(self.lines[0])):
-            for col in range(num_lines):
-                if pid >= num_players:
-                    break
+        col_counter = 0
+                
+        # ----
+        for col, line in enumerate(self.lines):
+            for row, player in enumerate(line):
+                pid = player.id
                 
                 # initialize player
                 p = DisplayObject(
                     '../assets/player.png',
-                    col * COL_GAP + 100, # NOTE: i have no clue why switching x and y works
+                    col * COL_GAP + 100,
                     row * ROW_GAP + 100, 
                     pid
                 )
                 
                 # set player tint
-                if pid in player_tints.keys():
-                    p.set_tint(player_tints[pid])
+                if pid in obj_tints.keys():
+                    p.set_tint(obj_tints[pid])
+                else:
+                    # rotate players colors for enhanced visibility
+                    if col_counter == 0:
+                        p_color = 'darkslategray'
+                    else:
+                        p_color = f'darkslategray{col_counter}'
+                    col_counter +=1 
+                    col_counter %= 4
+                    
+                    p.set_tint(pygame.colordict.THECOLORS[p_color])
                     
                 # add player to the correct line and the group of players
                 player_dict[pid] = p
                 players.add(p)
-                
-                pid += 1
         
         # initialize ball
         ball_x, ball_y = player_dict[self.get_id(self.start_line)].rect.center
         ball = DisplayObject('../assets/ball.png', ball_x, ball_y, -1)
-        ball.set_tint(ball_tint)
+        if -1 in obj_tints.keys():
+            ball.set_tint(obj_tints[-1])
         
         return players, player_dict, ball
     
@@ -242,5 +245,31 @@ class Display():
         
         return False
     
+    def verify_obj_tints(self, obj_tints: dict[int, RGB | RGBA | str]):
+        '''Verify that the obj_tints dictionary is correctly formatted. It 
+        must contain integer keys and values that are either RGB, RGBA, or
+        a valid color name from pygame.colordict.THECOLORS. The keys must 
+        be found in the player_dict or be -1 for the ball.
+
+        Args:
+            obj_tints (dict[int, RGB  |  RGBA  |  str]): The dictionary of object tints.
+        '''
+        if not obj_tints:
+            return
+        
+        for k, v in obj_tints.items():
+            assert isinstance(k, int), f"ID must be an integer. Got {k} instead."
+            
+            if isinstance(v, list):
+                assert len(v) in [3, 4], f"Must be RGB or RGBA. Got {v} instead."
+                for i in v:
+                    assert isinstance(i, int) and i in range(256), f"Must be valid RGB or RGBA. Got {v} instead."
+                obj_tints[k] = tuple(v)
+            elif isinstance(v, str):
+                assert v in pygame.colordict.THECOLORS, f"Must be a valid color name from pygame.colordict.THECOLORS. Got {v} instead."
+                obj_tints[k] = pygame.colordict.THECOLORS[v]
+            else:
+                raise AssertionError(f"Value must be a list or string. Got {v} instead.")
+        
     def close(self):
         pygame.quit()

example-configs/colors.yaml
@@ -1,0 +1,13 @@
+# colors.yaml
+# You can use the color names from the pygame.colordict.THECOLORS dictionary, RGB, or RGBA.
+# RGB/RGBA values must be between 0 and 255. They must also be passed in as a list as shown below.
+-2: azure4 # background
+-1:  # ball
+  - 255
+  - 165
+  - 0
+0: # player 0
+  - 0
+  - 0
+  - 0
+  - 255

example-configs/line_config.yaml
@@ -1,0 +1,9 @@
+# line_config.yaml
+# key is the line id and the value is the number of players in that line.
+0: 3
+1: 2
+2: 2
+3: 7
+4: 1
+
+start_line: 1

src/drill.py
@@ -11,24 +11,44 @@ class Drill:
         num_lines: int = 4, 
         num_players: int = 15, 
         starting_line: int = 0, 
+        line_config: dict[int, int] | None = None,
     ):
-        '''Constructs a drill.
+        '''Constructs a drill. Note that the number of lines must be 
+        less than the number of players, the starting line must have 
+        at least two players, and the starting line must be less than 
+        number of lines. If the line_config is provided, num_lines and 
+        num_players will be ignored. Starting line will be ignored if 
+        it is provided in the line_config.
 
         Args:
             num_lines (int, optional): Number of lines in the drill. Defaults to 4.
             num_players (int, optional): Total number of players in the drill. Defaults to 15.
             starting_line (int, optional): The index of the line that starts with the ball. Defaults to 0.
+            line_config (dict[int, int] | None, optional): A dictionary where the key is the line id and the value 
+            is the number of players in that line. Defaults to None.
         '''
         assert num_lines < num_players + 1, "Number of lines must be less than number of players + 1"
         assert num_lines > 1, "Number of lines must be greater than 1"
         assert starting_line < num_lines, "Starting line must be less than number of lines"
-        self.num_lines = num_lines
-        self.num_players = num_players
-        self.direction = 'right'
+        self.verify_line_config(line_config)
+        
         self.starting_line = self.line_with_ball = starting_line
-        self.has_oscillators = False # Flag to check if any player has oscillated
         self.moving_players = set() # List of players whose paths have been changed
-        self.lines, self.players = self._init_lines()
+        self.has_oscillators = False # Flag to check if any player has oscillated
+        self.direction = 'right'
+        
+        if line_config:
+            init_result = self._init_lines_from_config(line_config)
+            self.num_lines = init_result[0]
+            self.num_players = init_result[1]
+            self.lines = init_result[2]
+            self.players = init_result[3]
+        else:
+            self.num_lines = num_lines
+            self.num_players = num_players
+            self.lines, self.players = self._init_lines_default()
+            
+        
         assert len(self.lines[self.starting_line]) > 1, "Starting line must have more than one player."
         
     def run_visible(
@@ -69,12 +89,12 @@ class Drill:
         if verbose:
             self.print_oscillations(total_passes)
         
-    def _init_lines(self) -> tuple[list[Player], set[Player]]:
+    def _init_lines_default(self) -> tuple[list[Player], set[Player]]:
         '''Instantiate the players and distribute them into lines from left to right.
         
         Returns:
             tuple[list[Player], set[Player]]: A list of lines with players and a set of all players.
-        '''
+        '''        
         lines = [[] for _ in range(self.num_lines)]
         players = set()
         pid = 0
@@ -98,6 +118,43 @@ class Drill:
                 
         lines[self.starting_line][0].has_ball = True
         return lines, players
+    
+    def _init_lines_from_config(
+        self, 
+        line_config: dict[int, int]
+    ) -> tuple[list[Player], set[Player]]:
+        '''Initialize the players and distribute them 
+        into lines based on the line configuration.
+
+        Args:
+            line_config (dict[int, int]): Line configuration where the key is 
+            the line id and the value is the number of players in that line.
+
+        Returns:
+            tuple[int, int, list[Player], set[Player]]: The number of lines, 
+            the number of players, a list of lines with players, and a set of all players.
+        '''
+        lines = []
+        players = set()
+        
+        # get and check the keys representing the line ids
+        keys = list(line_config.keys())
+        keys.sort()
+        assert all(keys[i] + 1 == keys[i + 1] for i in range(len(keys) - 1) for _ in [keys]), "Line ids must be consecutive."
+        
+        pid = 0
+        for k in keys:
+            line = [] 
+            for _ in range(line_config[k]):
+                p = Player(player_id=pid, curr_line=k)
+                line.append(p)
+                players.add(p)
+                pid += 1
+            lines.append(line)
+            
+        lines[self.starting_line][0].has_ball = True
+                
+        return len(keys), pid, lines, players
         
     def is_last_line(self):
         '''Check if the line with the ball is in one of the last lines.
@@ -183,3 +240,23 @@ class Drill:
                 utils.printRed(s) if percentage >= 2 else utils.printGreen(s)
             else:
                 utils.printGreen(f"Player {player.id} did not oscillate.")
+                
+    def verify_line_config(self, line_config: dict[int, int] | None):
+        '''Verify that the line configuration is valid. The line id must be an integer
+        greater than or equal to 0 and the number of players in a line must be greater than 0.
+        The key 'start_line' is also allowed. 
+
+        Args:
+            line_config (dict[int, int] | None): The line configuration to verify.
+
+        Raises:
+            KeyError: If an invalid key is found in the line configuration.
+        '''
+        if not line_config:
+            return
+        
+        for key, value in line_config.items():
+            assert isinstance(key, int), "Line id must be an integer."
+            assert isinstance(value, int), "Number of players in a line must be an integer."
+            assert key >= 0, "Invalid key in line configuration. Must be a non-negative integer or 'start_line'."
+            assert value > 0, "Number of players in a line must be greater than 0."

src/simulation.py
@@ -1,4 +1,5 @@
 import argparse
+import yaml
 
 from display import Display
 from drill import Drill
@@ -10,13 +11,14 @@ FRAME_RATE = 60
 
 def parse_args() -> argparse.Namespace:
     '''Parse command line arguments. Setting the example flag will override 
-    all other arguments except speed. Example 1 takes precedence over example 2.
+    all other arguments except speed and colors. Example 1 takes precedence over example 2.
 
     Returns:
         Namespace: The parsed arguments. 
     '''
     parser = argparse.ArgumentParser(description='Run a simulation of infinite passing.')
     
+    # --- Simulation parameters ---
     parser.add_argument("--players", help="Number of players", default=10, type=int)
     parser.add_argument("--lines", help="Number of lines", default=4, type=int)
     parser.add_argument("--start-line", help="Starting line", default=0, type=int)
@@ -27,14 +29,67 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--hidden", help="Do not display the drill", action="store_false", dest="display", default=True)
     parser.add_argument("--step", help="Step through the simulation using space bar.", action="store_true", default=False)
     parser.add_argument("--debug", help="Display player ids insteda of images.", action="store_true", default=False)
-        
+    
+    # config files
+    parser.add_argument("--colors", help="Yaml file with display object colors based on id. Ball id is -1. Background is -2.", default="../example-configs/colors.yaml")
+    '''
+    # colors.yaml
+    # You can use the color names from the pygame.colordict.THECOLORS dictionary, RGB, or RGBA.
+    # RGB/RGBA values must be between 0 and 255. They must also be passed in as a list as shown below.
+    -1: # the ball
+        - 255
+        - 0
+        - 0
+    0: 
+        - 0
+        - 255
+        - 0
+        - 100
+    1: blueviolet
+    '''
+    
+    parser.add_argument("--line-config", help="Yaml file with line configuration. This will override --lines and --players.", default="../example-configs/line_config.yaml")
+    '''
+    # line_config.yaml
+    # key is the line id and the value is the number of players in that line.
+    0: 5
+    1: 5
+    2: 1 
+    3: 3
+    
+    # if you want to override the --start-line flag, add a key 'start_line'
+    start_line: 0
+    '''
+    
     # examples 
     parser.add_argument("--example1", help="Run example 1", action="store_true")
     parser.add_argument("--example2", help="Run example 2", action="store_true")
 
+    # --- Parse arguments ---
     args = parser.parse_args()
-    args.player_tints = {}
+    args.obj_tints = {}
+    args.lines_dict = None
+    args.bg_color = BG_COLOR
     
+    # --- Load config files ---
+    if args.colors:
+        with open(args.colors, "r") as f:
+            cols = yaml.safe_load(f)
+            if cols is not None:
+                args.obj_tints = cols
+                assert isinstance(args.obj_tints, dict), f"Colors file must be a dictionary."
+                if -2 in args.obj_tints:
+                    args.bg_color = args.obj_tints[-2]
+    if args.line_config:
+        with open(args.line_config, "r") as f:
+            args.lines_dict = yaml.safe_load(f)
+            if args.lines_dict is not None:
+                assert isinstance(args.lines_dict, dict), "Line config file must be a dictionary."
+                if "start_line" in args.lines_dict:
+                    args.start_line = args.lines_dict["start_line"]
+                args.lines_dict.pop("start_line", None)
+    
+    # --- Examples ---
     if args.example1:
         args.players = 5
         args.display = True
@@ -42,8 +97,9 @@ def parse_args() -> argparse.Namespace:
         args.start_line = 0
         args.passes = 5
         args.verbose = True
-        args.player_tints = {1: THECOLORS["red"]}
+        args.obj_tints = {1: THECOLORS["red"]}
         args.step = False
+        args.lines_dict = None
     elif args.example2:
         args.players = 7
         args.display = True
@@ -51,8 +107,9 @@ def parse_args() -> argparse.Namespace:
         args.start_line = 0
         args.passes = 9
         args.verbose = True
-        args.player_tints = {1: THECOLORS["green"]}
+        args.obj_tints = {1: THECOLORS["green"]}
         args.step = False
+        args.lines_dict = None
         
     return args
                 
@@ -65,22 +122,22 @@ if __name__ == "__main__":
         num_lines=args.lines,
         num_players=args.players, 
         starting_line=args.start_line,
-    )
-    
-    display = Display(
-        start_line=args.start_line,
-        lines=drill.lines,
-        speed=args.speed,
-        win_size=WIN_SIZE,
-        player_tints=args.player_tints,
-        ball_tint=None,
-        bg_color=BG_COLOR,
-        frame_rate=FRAME_RATE,
-        step=args.step,
-        debug=args.debug,
+        line_config=args.lines_dict,
     )
     
     if args.display:
+        display = Display(
+            start_line=args.start_line,
+            lines=drill.lines,
+            speed=args.speed,
+            win_size=WIN_SIZE,
+            obj_tints=args.obj_tints,
+            bg_color=args.bg_color,
+            frame_rate=FRAME_RATE,
+            step=args.step,
+            debug=args.debug,
+        )
+        
         drill.run_visible(args.passes, display, args.verbose)
     else:
         drill.run_hidden(args.passes, args.verbose)

src/utils.py
@@ -123,7 +123,11 @@ def tint_image(surface: pygame.Surface, color: ColorValue) -> pygame.Surface:
     for x in range(w):
         for y in range(h):
             a = surface.get_at((x, y))[3]
-            surface.set_at((x, y), pygame.Color(r, g, b, a))
+            try:
+                surface.set_at((x, y), pygame.Color(r, g, b, a))
+            except ValueError as e:
+                raise ValueError(f"Invalid color value: {color}.") from e
+                
             
 def get_new_dims(target_width: int, surface: pygame.Surface) -> Tuple[int, int]:
     '''Calculate the new dimensions of the image based on the target width. 

commit dbb941c83c9d49d68add9a5fc945ea50dc6fa2e7

fix bug with example caused by previous commit c262939c750bb89d49a0c03fe0b0cc1a622832f0

src/display.py
@@ -268,6 +268,10 @@ class Display():
             elif isinstance(v, str):
                 assert v in pygame.colordict.THECOLORS, f"Must be a valid color name from pygame.colordict.THECOLORS. Got {v} instead."
                 obj_tints[k] = pygame.colordict.THECOLORS[v]
+            elif isinstance(v, tuple):
+                assert len(v) in [3, 4], f"Must be RGB or RGBA. Got {v} instead."
+                for i in v:
+                    assert isinstance(i, int) and i in range(256), f"Must be valid RGB or RGBA. Got {v} instead."
             else:
                 raise AssertionError(f"Value must be a list or string. Got {v} instead.")
         